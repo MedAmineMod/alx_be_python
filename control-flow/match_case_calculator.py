274ab4d79831f9e2b2d517ec5a2358a6e52f2656
MedAmineMod <modni.amine@outlok.com>
2024-09-23 18:01:31 +0100

    ..

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -8,11 +8,13 @@ op = input("Choose the operation (+, -, *, /): ")
 num1 = float(num1)
 num2 = float(num2)
 
-if op == "+" : print(f"The result is {num1 + num2}.")  
-elif op == "-" : print(f"The result is {num1 - num2}.")  
-elif op == "*" : print(f"The result is {num1 * num2}.")  
-elif op == "/" : 
-    if num1 > num2 :
-        print("Cannot divide by zero.")
-    else :    
-        print(f"The result is {num1 / num2}.")  
+
+match op:
+    case  "+" : print(f"The result is {num1 + num2}.")
+    case  "-" : print(f"The result is {num1 - num2}.")  
+    case  "*" : print(f"The result is {num1 * num2}.")  
+    case  "/" : 
+        if num1 > num2 :
+            print("Cannot divide by zero.")
+        else :    
+            print(f"The result is {num1 / num2}.")  
