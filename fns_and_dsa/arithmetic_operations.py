21ffcba4ac80e283b70b6f0eb41f081765dee017
MedAmineMod <modni.amine@outlok.com>
2024-09-23 19:45:13 +0100

    ,,

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,22 @@
 def perform_operation(num1, num2, operation): 
     
-    match operation : 
-        case "add" :
-            return num1 + num2
-        case  "subtract": 
-            return num1 - num2
-        case  "multiply" :
-            return num1 * num2
-        case  "devide" :
-            if(num2 != 0) :
-                return num1 / num2
-        case _:
-            print("Not allowed operation")
+    if operation == "add":
+        return num1 + num2
+    
+    elif operation == "subtract":
+        return num1 - num2
+    
+    elif operation == "multiply":
+        return num1 * num2
+    
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            print("Cannot divide by zero")
             
+    else:
+        print("Not allowed operation")
         
     
     
