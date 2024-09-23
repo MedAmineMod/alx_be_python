2a482f5dd05a7630dc3679a2e3b5a9aef80c7709
Amine Modni <35262570+MedAmineMod@users.noreply.github.com>
2024-09-23 00:42:53 +0000

    ..

--- basic_operations.py
+++ basic_operations.py
@@ -1,6 +1,9 @@
 number1 = 5 
 number2 = 10 
 
-print(f"Sum of {number1} and {number2} is { number1 + number2}")
+print(f"Addition of {number1} and {number2} is { number1 + number2}")
 
-print(f"Product of {number1} and {number2} is { number1 * number2}")
+print(f"Subtraction of {number1} and {number2} is { number1 - number2}")
+
+
+print(f"Multiplication of {number1} and {number2} is { number1 * number2}")
