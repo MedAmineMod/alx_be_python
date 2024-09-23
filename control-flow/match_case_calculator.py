9227e90f33a1ebc96cbaf405996e9d18e39f49a7
MedAmineMod <modni.amine@outlok.com>
2024-09-23 18:04:05 +0100

    ..

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -14,7 +14,7 @@ match op:
     case  "-" : print(f"The result is {num1 - num2}.")  
     case  "*" : print(f"The result is {num1 * num2}.")  
     case  "/" : 
-        if num1 > num2 :
+        if  num2 == 0  :
             print("Cannot divide by zero.")
         else :    
             print(f"The result is {num1 / num2}.")  
