c5d178f333339e3dde5bf6308aa892a452f714f2
MedAmineMod <modni.amine@outlok.com>
2024-09-23 18:22:14 +0100

    ..

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,9 +1,9 @@
-number = input("Enter the size of the pattern: ")
+number = int(input("Enter the size of the pattern: "))
 
 line = ""
 
-for n in range(0,int(number))  : 
+for n in range(0,number) : 
     line = line + "*"  
-for n in range(0,int(number)) :
+for n in range(0,number) :
     print(line)
    
