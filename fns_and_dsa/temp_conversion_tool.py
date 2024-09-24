b000beab61ab4e7ff3c1bb04daf22548785b82c4
MedAmineMod <modni.amine@outlok.com>
2024-09-24 14:25:59 +0100

    ..

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,31 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR  = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR  = 9/5
+
+
+
+def convert_to_celsius (fahrenheit) :
+    
+    calc =    (float(fahrenheit) - 32 )  * FAHRENHEIT_TO_CELSIUS_FACTOR
+    
+    print(f"{fahrenheit}°F is {calc}°C")
+
+
+def convert_to_fahrenheit (celsius) :
+     
+    calc =    (float(celsius)  * CELSIUS_TO_FAHRENHEIT_FACTOR )  + 32
+    
+    print(f"{celsius}°C is {calc}°F")
+
+    
+    
+ 
+ 
+
+temp = input("Enter the temperature to convert: " )
+
+type = input("Is this temperature in Celsius or Fahrenheit? (C/F): " )
+
+if type == "C" : 
+    convert_to_fahrenheit(temp)
+else :
+    convert_to_celsius(temp)
