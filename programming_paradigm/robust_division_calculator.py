002bb887773d5bd5535632356400e1bbd6acc851
MedAmineMod <modni.amine@outlook.com>
2024-10-06 19:41:36 +0100

    ..

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,18 @@
+
+def safe_divide(numerator, denominator):
+   
+    try : 
+        float_n =  float(numerator)
+        float_d =  float(denominator)
+    except (ValueError):
+        return ValueError("Error: Please enter numeric values only.")
+    
+
+    try :
+     res  = float_n/float_d
+    
+    except (ZeroDivisionError)  :
+        return  ZeroDivisionError("Error: Cannot divide by zero.")
+    else :   
+        return    res
+
