b3b1a94aae8264737a0bc5292eef3085c495c637
MedAmineMod <modni.amine@outlook.com>
2024-10-06 18:20:10 +0100

    ..

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+class BankAccount :
+   
+    def __init__(self , account_balance = 0  ):
+        self.account_balance  =  account_balance 
+    
+    def deposit(self , amount) :
+        self.account_balance += amount 
+
+    def withdraw(self , amount) :
+
+        if(self.account_balance >= amount ) : 
+            self.account_balance -= amount
+            return True 
+        else :
+            return False 
+
+    def display_balance(self) :
+        print(f"Current Balance: ${self.account_balance}")
+
+
+# bank_account =  BankAccount()
+# bank_account.deposit(88)
+# bank_account.withdraw(88)
+
+# bank_account.display_balance()
+ 
