8d123333297981faef30abea4a0c5dbc837321ed
MedAmineMod <modni.amine@outlook.com>
2024-10-06 23:10:08 +0100

    ..

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -11,19 +11,32 @@ class Book:
 
 class Library :
 
-    _books = {}
+    def __init__(self  ):
+        self._books = {}
 
     def add_book(self , book ) : 
         title =  book.title 
         self._books[title] = book
+        
+
        
     
     def check_out_book (self , title) : 
         self._books[title]._is_checked_out = True
+        if (self._books[title]._is_checked_out  == True ) :
+            print(self._books[title].title + " IS OUT" )
+        else : 
+            print(self._books[title].title + " IS IN" )
         
 
     def return_book (self , title) : 
         self._books[title]._is_checked_out = False
+
+        if (self._books[title]._is_checked_out  == True ) :
+            print(self._books[title].title + " IS OUT" )
+        else : 
+            print(self._books[title].title + " IS IN" )
+            
 
     def list_available_books (self) :
         for title in self._books :
