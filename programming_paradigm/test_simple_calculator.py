fde42caea24197030923deb90b6fa7cf8f8f6b30
MedAmineMod <modni.amine@outlook.com>
2024-10-06 20:04:42 +0100

    ..

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,16 +8,16 @@ class TestSimpleCalculator(unittest.TestCase) :
 
     def test_addition(self) :
         result = simple.add(5 ,5 )
-        self.assertEqual(result , 10)
+        self.assertEqual(self , result , 10)
 
     def test_subtraction(self) :
         result = simple.subtract(5 ,5 )
-        self.assertEqual(result , 0)
+        self.assertEqual(self ,  result , 0)
     
     def test_multiply(self) :
         result = simple.multiply(5 ,5 )
-        self.assertEqual(result , 25)
+        self.assertEqual(self ,  result , 25)
 
     def test_divide(self) :
         result = simple.divide(5 ,5 )
-        self.assertEqual(result , 1)
+        self.assertEqual(self ,  result , 1)
